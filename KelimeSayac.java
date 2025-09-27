67e46986ad98ce353970e562369542432daef32e
esmaelifozturk <esmaelif2626@gmail.com>
2025-09-27 13:33:48 +0300

    Add files via upload

---
+++ KelimeSayac.java
@@ -0,0 +1,25 @@
+package projeler;
+
+import java.util.Scanner;
+
+public class KelimeSayac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("enter the text: ");
+		String text = scan.nextLine();
+		String pieces[] = text.split(" ");
+		
+		int counter = 0;
+		for(String textPieces : pieces) {
+			System.out.println(textPieces);
+			counter++;
+		}
+		
+		System.out.println(counter);
+
+	}
+
+}
